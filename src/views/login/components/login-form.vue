a9e21732464b8c3446bab1323e038cda9e6b26cd
yantingwei <akkoyan@qq.com>
2023-11-17 17:20:36 +0800

    :lipstick: feat: 优化登录页面 UI

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -1,65 +1,90 @@
 <template>
   <div class="login-form-wrapper">
-    <div class="login-form-title">{{ $t('login.form.title') }}</div>
-    <div class="login-form-sub-title">{{ $t('login.form.subTitle') }}</div>
-    <div class="login-form-error-msg">{{ errorMessage }}</div>
-    <a-form
-      ref="loginForm"
-      :model="userInfo"
-      class="login-form"
-      layout="vertical"
-      @submit="handleSubmit"
-    >
-      <a-form-item
-        :rules="[{ required: true, message: $t('login.form.userName.errMsg') }]"
-        :validate-trigger="['change', 'blur']"
-        field="userAccount"
-        hide-label
-      >
-        <a-input
-          v-model="userInfo.userAccount"
-          :placeholder="$t('login.form.userName.placeholder')"
-        >
-          <template #prefix>
-            <icon-user />
-          </template>
-        </a-input>
-      </a-form-item>
-      <a-form-item
-        :rules="[{ required: true, message: $t('login.form.password.errMsg') }]"
-        :validate-trigger="['change', 'blur']"
-        field="userPassword"
-        hide-label
-      >
-        <a-input-password
-          v-model="userInfo.userPassword"
-          :placeholder="$t('login.form.password.placeholder')"
-          allow-clear
-        >
-          <template #prefix>
-            <icon-lock />
-          </template>
-        </a-input-password>
-      </a-form-item>
-      <a-space :size="16" direction="vertical">
-        <div class="login-form-password-actions">
-          <a-checkbox
-            :model-value="loginConfig.rememberPassword"
-            checked="rememberPassword"
-            @change="setRememberPassword as any"
-          >
-            {{ $t('login.form.rememberPassword') }}
-          </a-checkbox>
-          <a-link>{{ $t('login.form.forgetPassword') }}</a-link>
-        </div>
-        <a-button :loading="loading" html-type="submit" long type="primary">
-          {{ $t('login.form.login') }}
-        </a-button>
-        <a-button class="login-form-register-btn" long type="text">
-          {{ $t('login.form.register') }}
-        </a-button>
-      </a-space>
-    </a-form>
+    <a-card class="login-form-out-card">
+      <a-row align="center">
+        <a-col :span="14">
+          <img src="src/assets/images/login/login-bg.svg" style="width: 100%" />
+        </a-col>
+        <a-col :span="10">
+          <a-card :bordered="false">
+            <div class="login-form-title">{{ $t('login.form.title') }}</div>
+            <div class="login-form-sub-title"
+              >{{ $t('login.form.subTitle') }}
+            </div>
+            <div class="login-form-error-msg">{{ errorMessage }}</div>
+            <a-form
+              ref="loginForm"
+              :model="userInfo"
+              class="login-form"
+              layout="vertical"
+              @submit="handleLogin"
+            >
+              <a-form-item
+                :rules="[
+                  {
+                    required: true,
+                    message: $t('login.form.userName.errMsg'),
+                  },
+                ]"
+                :validate-trigger="['change', 'blur']"
+                field="userAccount"
+                hide-label
+              >
+                <a-input
+                  v-model="userInfo.userAccount"
+                  :placeholder="$t('login.form.userName.placeholder')"
+                >
+                  <template #prefix>
+                    <icon-user />
+                  </template>
+                </a-input>
+              </a-form-item>
+              <a-form-item
+                :rules="[
+                  {
+                    required: true,
+                    message: $t('login.form.password.errMsg'),
+                  },
+                ]"
+                :validate-trigger="['change', 'blur']"
+                field="userPassword"
+                hide-label
+              >
+                <a-input-password
+                  v-model="userInfo.userPassword"
+                  :placeholder="$t('login.form.password.placeholder')"
+                  allow-clear
+                >
+                  <template #prefix>
+                    <icon-lock />
+                  </template>
+                </a-input-password>
+              </a-form-item>
+              <a-space :size="16" direction="vertical">
+                <div class="login-form-password-actions">
+                  <a-checkbox
+                    :model-value="loginConfig.rememberPassword"
+                    checked="rememberPassword"
+                    @change="setRememberPassword as any"
+                  >
+                    {{ $t('login.form.rememberPassword') }}
+                  </a-checkbox>
+                  <a-link>{{ $t('login.form.forgetPassword') }}</a-link>
+                </div>
+                <a-button
+                  :loading="loading"
+                  html-type="submit"
+                  long
+                  type="primary"
+                >
+                  {{ $t('login.form.login') }}
+                </a-button>
+              </a-space>
+            </a-form>
+          </a-card>
+        </a-col>
+      </a-row>
+    </a-card>
   </div>
 </template>
 
@@ -90,7 +115,7 @@
     userPassword: loginConfig.value.userPassword,
   });
 
-  const handleSubmit = async ({
+  const handleLogin = async ({
     errors,
     values,
   }: {
@@ -123,6 +148,9 @@
       }
     }
   };
+  const handleRegister = () => {
+    console.log('register');
+  };
   const setRememberPassword = (value: boolean) => {
     loginConfig.value.rememberPassword = value;
   };
@@ -131,7 +159,18 @@
 <style lang="less" scoped>
   .login-form {
     &-wrapper {
-      width: 320px;
+      height: 50vh;
+      width: 60vw;
+    }
+
+    &-out-card {
+      width: 100%;
+      height: 100%;
+      border-radius: 16px;
+      box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 
     &-title {
@@ -162,6 +201,7 @@
 
     &-register-btn {
       color: var(--color-text-3) !important;
+      text-align: center;
     }
   }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,6 +16,13 @@
   .container {
     display: flex;
     height: 100vh;
+    //background: linear-gradient(
+    //  135deg,
+    //  rgba(24, 144, 255, 0.7) 0%,
+    //  rgba(103, 58, 183, 0.5) 90%
+    //);
+    background-image: url("https://gw.alipayobjects.com/zos/rmsportal/TVYTbAXWheQpRcWDaDMu.svg");
+    background-color: var(--color-fill-1);
 
     .content {
       position: relative;
